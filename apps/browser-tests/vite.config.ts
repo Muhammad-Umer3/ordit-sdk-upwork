ea97c0d9280cee2e1bd06b9742875eef90768519
Muhammad Umer <umerasif924@gmail.com>
2024-02-07 17:46:41 +0500

    resolved react build issue

--- apps/browser-tests/vite.config.ts
+++ apps/browser-tests/vite.config.ts
@@ -4,14 +4,6 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 import * as packageJson from "./package.json";
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: Object.keys(packageJson.dependencies),
-    },
-    commonjsOptions: {
-      include: [/node_modules/],
-    },
-  },
   plugins: [
     react(),
     nodePolyfills({
